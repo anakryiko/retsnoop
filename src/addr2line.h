798e36ab5e392a33ccb57a367591543a38ab9586
Thinker Lee <thinker.li@gmail.com>
2021-11-12 12:14:21 -0800

    retsnoop: allow/deny/entry all functions defined in compile unit(s)
    By prefixing name(s) of compile unit(s) with ':', it finds out all
    functions defined in the compile unit(s), and add them to specified
    list of globs.
    
    Signed-off-by: Thinker Lee <thinker.li@gmail.com>

--- src/addr2line.h
+++ src/addr2line.h
@@ -9,11 +9,18 @@ struct a2l_resp
 	char line[512];
 };
 
+struct a2l_cu_resp
+{
+	char fname[128 - sizeof(void*)]; /* Reduce fragment */
+	void* address;
+};
+
 struct addr2line;
 
 struct addr2line *addr2line__init(const char *vmlinux, bool inlines);
 void addr2line__free(struct addr2line *a2l);
 
 int addr2line__symbolize(const struct addr2line *a2l, long addr, struct a2l_resp *resp);
+int addr2line__query_symbols(const struct addr2line *a2l, const char *compile_unit, struct a2l_cu_resp **resp_ret);
 
 #endif /* __ADDR2LINE_H */
