f82e0f83c7d385cc15d4b7f62ebc80d610d394f7
Andrii Nakryiko <andrii@kernel.org>
2022-08-05 15:46:22 -0700

    retsnoop: v0.9 release with --trace mode
    Bump retsnoop version to v0.9.
    
    Signed-off-by: Andrii Nakryiko <andrii@kernel.org>

--- src/retsnoop.c
+++ src/retsnoop.c
@@ -109,7 +109,7 @@ static struct env {
 	.stacks_map_sz = 1024,
 };
 
-const char *argp_program_version = "retsnoop v0.8.3";
+const char *argp_program_version = "retsnoop v0.9";
 const char *argp_program_bug_address = "Andrii Nakryiko <andrii@kernel.org>";
 const char argp_program_doc[] =
 "retsnoop tool shows kernel call stacks based on specified function filters.\n"
