9f9222611e618c4172f13a82e7bac6a99ac8b81f
Andrii Nakryiko <andrii@kernel.org>
2022-10-20 23:40:28 -0700

    retsnoop: switch to per-architecture vmlinux.h
    Add per-architecture vmlinux.h headers, wire them up in Makefile
    properly. These vmlinux.h headers are generated using
    scripts/gen-vmlinux-headers.sh and are much smaller than previous
    manually generated vmlinux.h header. Remove old x86_64-specific
    vmlinux.h, which is now replaced by much smaller one.
    
    Signed-off-by: Andrii Nakryiko <andrii@kernel.org>

--- src/mass_attach.bpf.c
+++ src/mass_attach.bpf.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-2-Clause
 /* Copyright (c) 2021 Facebook */
-#include "vmlinux.h"
+#include <vmlinux.h>
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
 #include <bpf/bpf_core_read.h>
